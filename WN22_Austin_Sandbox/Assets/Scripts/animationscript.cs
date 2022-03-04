4083ea8ae1f0114d21f93721914018cf77975cc2
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2022-03-03 21:32:19 -0700

    done lab march3rd

--- WN22_Austin_Sandbox/Assets/Scripts/animationscript.cs
+++ WN22_Austin_Sandbox/Assets/Scripts/animationscript.cs
@@ -6,27 +6,59 @@ public class animationscript : MonoBehaviour
 { 
     
     public Animator animate;
+    [SerializeField] string parameterName;
 
+    [SerializeField] bool hasTrigger = false;
+    [SerializeField] bool hasFloat= false;
+    [SerializeField] bool hasInt = false;
+    [SerializeField] bool hasBool = false;
+    
     public void AnimationTrigger()
-    {
-        if (!animate.GetBool("toggle"))
+    { 
+        if (hasTrigger) //for triggers
         {
-            animate.SetBool("toggle", true);
-            Debug.Log("yes");
+
+            animate.SetTrigger(parameterName);
+            Debug.Log("tigger set off");
         }
-        else
+       
+
+        if (hasBool) //for bools
         {
-            animate.SetBool("toggle", false);
-            Debug.Log("no");
+
+
+            if (!animate.GetBool(parameterName))
+            {
+                animate.SetBool(parameterName, true);
+                Debug.Log("yes");
+            }
+            else
+            {
+                animate.SetBool(parameterName, false);
+                Debug.Log("no");
+            }
+
         }
-        
+
+        if (hasInt)
+        {
+            Debug.Log("int");
+        }
+
+        if (hasFloat)
+        {
+            Debug.Log("float");
+        }
     }
+
+   
     // Start is called before the first frame update
     void Start()
     {
-        
        
-
+       
+    
+       
 
     }
 
